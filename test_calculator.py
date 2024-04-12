50d67036dbadc6fbf198e560d8e95a6934b25f10
N-19-dev <127797502+N-19-dev@users.noreply.github.com>
2024-04-12 15:30:17 +0200

    road to 10/10 in pylint

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,27 @@
+"""
+This module contains unit tests for the calculator module.
+"""
+
 from calculator import Calculator
 
 def test_addition():
+    """
+    Test addition function.
+    """
     calc = Calculator()
     assert calc.addition(5, 5) == 10
 
 def test_soustraction():
+    """
+    Test soustraction function.
+    """
     calc = Calculator()
     assert calc.soustraction(15, 5) == 10
 
 def test_division():
+    """
+    Test division function.
+    """
     calc = Calculator()
     assert calc.division(100, 10) == 10
 
